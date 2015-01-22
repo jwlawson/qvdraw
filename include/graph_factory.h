d122b350136edf0ff93611daf513060bbf88aeab
John Lawson <john@jwlawson.co.uk>
2015-01-22 14:28:41 +0000

    Adds copyright header.

--- include/graph_factory.h
+++ include/graph_factory.h
@@ -1,6 +1,20 @@
 /**
  * graph_factory.h
+ * Copyright 2014-2015 John Lawson
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
  * Contains functions to create graph objects from the matrix representation.
  */
 #pragma once
