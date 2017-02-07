ddb4d3ff7ad6a4774a44217f8bcdc85e4707eea4
John Lawson <john@jwlawson.co.uk>
2017-02-07 15:31:03 +0000

    Formats code.
    For some stupid reason the vim clang-format extension doesn't expand tabs,
    even when the style does.

--- src/qv2tex.cc
+++ src/qv2tex.cc
@@ -64,9 +64,9 @@ void end(std::ostream& os) {
   os << "\\end{document}%" << os.widen('\n');
 }
 void draw_quiver(std::ostream& os,
-		 const ogdf::Graph& graph,
-		 const ogdf::GraphAttributes& attr,
-		 const char* color = "black") {
+                 const ogdf::Graph& graph,
+                 const ogdf::GraphAttributes& attr,
+                 const char* color = "black") {
   ogdf::node n;
   os << "\\begin{tikzpicture}[x=\\qvsize,y=\\qvsize]" << os.widen('\n');
   forall_nodes(n, graph) {
@@ -94,20 +94,20 @@ void draw_quiver(std::ostream& os,
   forall_nodes(n, graph) {
     if (attr.labelNode(n).length() > 0) {
       os << "\\tkzLabelPoint[color=" << color << "](n" << n->index() << "){$"
-	 << attr.labelNode(n);
+         << attr.labelNode(n);
       os << "$}%" << os.widen('\n');
     }
   }
   os << "\\draw[" << color << "](current bounding box.south west)rectangle"
-			      "(current bounding box.north east);"
+                              "(current bounding box.north east);"
      << os.widen('\n');
   os << "\\end{tikzpicture}%" << os.widen('\n');
 }
 void box_quiver(std::ostream& os,
-		const std::string& name,
-		const ogdf::Graph& graph,
-		const ogdf::GraphAttributes& attr,
-		const char* color = "black") {
+                const std::string& name,
+                const ogdf::Graph& graph,
+                const ogdf::GraphAttributes& attr,
+                const char* color = "black") {
   os << "\\newsavebox{\\" << name << "}%" << os.widen('\n');
   os << "\\sbox{\\" << name << "}{%" << os.widen('\n');
   os << "\\scalebox{\\qvscale}{%" << os.widen('\n');
@@ -128,9 +128,9 @@ std::string int_to_str(int a) {
 }
 template <class M>
 void draw_multi_graph(std::ostream& os,
-		      const qvdraw::NodeMap<M>& map,
-		      const ogdf::Graph& graph,
-		      const ogdf::GraphAttributes& attr) {
+                      const qvdraw::NodeMap<M>& map,
+                      const ogdf::Graph& graph,
+                      const ogdf::GraphAttributes& attr) {
   ogdf::node node;
   std::vector<ogdf::node> nodes_to_remove;
   forall_nodes(node, graph) {
@@ -138,8 +138,8 @@ void draw_multi_graph(std::ostream& os,
     if (found != map.end()) {
       const M* mat = found->second;
       std::pair<std::shared_ptr<ogdf::Graph>,
-		std::shared_ptr<ogdf::GraphAttributes>>
-	  pair = qvdraw::graph_factory::graph(*mat);
+                std::shared_ptr<ogdf::GraphAttributes>>
+          pair = qvdraw::graph_factory::graph(*mat);
       ogdf::Graph& n_graph = *pair.first;
       ogdf::GraphAttributes& n_attr = *pair.second;
       qvlayout::layout(n_graph, n_attr);
@@ -156,12 +156,12 @@ void draw_multi_graph(std::ostream& os,
      << os.widen('\n');
   forall_nodes(node, graph) {
     if (std::find(nodes_to_remove.begin(), nodes_to_remove.end(), node) !=
-	nodes_to_remove.end()) {
+        nodes_to_remove.end()) {
       continue;
     }
     std::string name = "node" + int_to_str(node->index());
     os << "\\node[inner sep=0pt,outer sep=0pt]"
-	  " (n"
+          " (n"
        << node->index() << ") at (" << attr.x(node) << "," << attr.y(node)
        << "){\\usebox{\\node" << int_to_str(node->index()) << "}};"
        << os.widen('\n');
@@ -169,10 +169,10 @@ void draw_multi_graph(std::ostream& os,
   ogdf::edge e;
   forall_edges(e, graph) {
     if (std::find_if(nodes_to_remove.begin(), nodes_to_remove.end(),
-		     [&e](ogdf::node node) -> bool {
-		       return (e->source()->index() == node->index()) ||
-			      (e->target()->index() == node->index());
-		     }) != nodes_to_remove.end()) {
+                     [&e](ogdf::node node) -> bool {
+                       return (e->source()->index() == node->index()) ||
+                              (e->target()->index() == node->index());
+                     }) != nodes_to_remove.end()) {
       continue;
     }
     os << "\\draw[line width=.05pt";
@@ -183,9 +183,9 @@ void draw_multi_graph(std::ostream& os,
 }
 template <class M>
 void draw_green_multi_graph(std::ostream& os,
-			    const qvdraw::NodeMap<M>& map,
-			    const ogdf::Graph& graph,
-			    const ogdf::GraphAttributes& attr) {
+                            const qvdraw::NodeMap<M>& map,
+                            const ogdf::Graph& graph,
+                            const ogdf::GraphAttributes& attr) {
   ogdf::node node;
   std::vector<ogdf::node> nodes_to_remove;
   cluster::green_exchange::MultiArrowTriangleCheck chk;
@@ -194,17 +194,17 @@ void draw_green_multi_graph(std::ostream& os,
     if (found != map.end()) {
       const M* mat = found->second;
       std::pair<std::shared_ptr<ogdf::Graph>,
-		std::shared_ptr<ogdf::GraphAttributes>>
-	  pair = qvdraw::graph_factory::graph(*mat);
+                std::shared_ptr<ogdf::GraphAttributes>>
+          pair = qvdraw::graph_factory::graph(*mat);
       ogdf::Graph& n_graph = *pair.first;
       ogdf::GraphAttributes& n_attr = *pair.second;
       qvlayout::layout(n_graph, n_attr);
       if (chk(mat, 0)) {
-	box_quiver(os, "node" + int_to_str(node->index()), n_graph, n_attr,
-		   "blue");
+        box_quiver(os, "node" + int_to_str(node->index()), n_graph, n_attr,
+                   "blue");
       } else {
-	box_quiver(os, "node" + int_to_str(node->index()), n_graph, n_attr,
-		   "red");
+        box_quiver(os, "node" + int_to_str(node->index()), n_graph, n_attr,
+                   "red");
       }
     } else {
       /* Node is not a quiver/seed.
@@ -218,12 +218,12 @@ void draw_green_multi_graph(std::ostream& os,
      << os.widen('\n');
   forall_nodes(node, graph) {
     if (std::find(nodes_to_remove.begin(), nodes_to_remove.end(), node) !=
-	nodes_to_remove.end()) {
+        nodes_to_remove.end()) {
       continue;
     }
     std::string name = "node" + int_to_str(node->index());
     os << "\\node[inner sep=0pt,outer sep=0pt]"
-	  " (n"
+          " (n"
        << node->index() << ") at (" << attr.x(node) << "," << attr.y(node)
        << "){\\usebox{\\node" << int_to_str(node->index()) << "}};"
        << os.widen('\n');
@@ -231,15 +231,15 @@ void draw_green_multi_graph(std::ostream& os,
   ogdf::edge e;
   forall_edges(e, graph) {
     if (std::find_if(nodes_to_remove.begin(), nodes_to_remove.end(),
-		     [&e](ogdf::node node) -> bool {
-		       return (e->source()->index() == node->index()) ||
-			      (e->target()->index() == node->index());
-		     }) != nodes_to_remove.end()) {
+                     [&e](ogdf::node node) -> bool {
+                       return (e->source()->index() == node->index()) ||
+                              (e->target()->index() == node->index());
+                     }) != nodes_to_remove.end()) {
       continue;
     }
     os << "\\draw[line width=.05pt";
     if (chk(map.find(e->source())->second, 0) &&
-	chk(map.find(e->target())->second, 0)) {
+        chk(map.find(e->target())->second, 0)) {
       os << ",blue";
     }
     os << "](n" << e->source()->index() << ") -- (n" << e->target()->index()
@@ -289,7 +289,7 @@ void output_multi_graph(const Graph& multi_gr, std::ostream& os) {
 void usage() {
   std::cout << "qv2tex -l [-n number] [-q|m|g|e quiver]" << std::endl;
   std::cout << "Takes a qv matrix and outputs the TeX to draw the quiver."
-	    << std::endl;
+            << std::endl;
   std::cout << "   -q Draw a single quiver" << std::endl;
   std::cout << "   -m Draw the move graph of a quiver" << std::endl;
   std::cout << "   -g Draw the quiver graph of a quiver" << std::endl;
@@ -298,7 +298,7 @@ void usage() {
       << std::endl;
   std::cout << "   -l Draw the labelled exchange/quiver graph" << std::endl;
   std::cout << "   -n Limit the number of seeds computed to given number"
-	    << std::endl;
+            << std::endl;
   std::cout
       << "   -r Don't compute mutations which do not lead to green sequences"
       << std::endl;
@@ -315,41 +315,41 @@ int main(int argc, char* argv[]) {
   while ((c = getopt(argc, argv, "c:q:m:g:e:ln:r")) != -1) {
     switch (c) {
       case 'c':
-	func = Func::cartan;
-	mat_str = optarg;
-	break;
+        func = Func::cartan;
+        mat_str = optarg;
+        break;
       case 'q':
-	func = Func::quiver;
-	mat_str = optarg;
-	break;
+        func = Func::quiver;
+        mat_str = optarg;
+        break;
       case 'm':
-	func = Func::move;
-	mat_str = optarg;
-	break;
+        func = Func::move;
+        mat_str = optarg;
+        break;
       case 'g':
-	func = Func::graph;
-	mat_str = optarg;
-	break;
+        func = Func::graph;
+        mat_str = optarg;
+        break;
       case 'e':
-	func = Func::exchange;
-	mat_str = optarg;
-	break;
+        func = Func::exchange;
+        mat_str = optarg;
+        break;
       case 'l':
-	labelled = true;
-	break;
+        labelled = true;
+        break;
       case 'n':
-	limit = std::stoul(optarg);
-	break;
+        limit = std::stoul(optarg);
+        break;
       case 'r':
-	green = true;
-	break;
+        green = true;
+        break;
       case '?':
-	usage();
-	return 1;
-	;
+        usage();
+        return 1;
+        ;
       default:
-	usage();
-	return 1;
+        usage();
+        return 1;
     }
   }
   if (func == Func::unset) {
@@ -360,8 +360,8 @@ int main(int argc, char* argv[]) {
   if (func == Func::quiver) {
     cluster::IntMatrix matrix(mat_str);
     std::pair<std::shared_ptr<ogdf::Graph>,
-	      std::shared_ptr<ogdf::GraphAttributes>>
-	pair = qvdraw::graph_factory::graph(matrix);
+              std::shared_ptr<ogdf::GraphAttributes>>
+        pair = qvdraw::graph_factory::graph(matrix);
     ogdf::Graph& graph = *pair.first;
     ogdf::GraphAttributes& attr = *pair.second;
     qvlayout::layout(graph, attr);
@@ -425,12 +425,12 @@ int main(int argc, char* argv[]) {
 
     if (!init_cartan_iter.has_next()) {
       std::cerr << "Quiver has no fully compatible matrices"
-		<< std::cerr.widen('\n');
+                << std::cerr.widen('\n');
       return 5;
     }
 
     refl::cartan_exchange::CartanQuiver initial{m, init_cartan_iter.next(),
-						true};
+                                                true};
 
     refl::CartanExchangeGraph graph(initial, m.num_rows(), limit);
     qv2tex::output_multi_graph<M>(graph, os);
